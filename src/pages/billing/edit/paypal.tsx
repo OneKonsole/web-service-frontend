f930e394013137facfdf0a23bf38fc783b3691cf
Wolodia Zdetovetzky <wolozdeto@gmail.com>
2023-12-11 08:17:17 +0100

    feat: add paypal login

--- src/pages/billing/edit/paypal.tsx
+++ src/pages/billing/edit/paypal.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import PanelLayout from "@components/layout/PanelLayout.tsx";
 import {PayPalInfo} from "@/type.ts";
 import {getUserBillingPayPalInfo} from "@/requests/billingRequests.ts";
+import {PayPalButtons, PayPalScriptProvider} from "@paypal/react-paypal-js";
+import {PayPalScriptOptions} from "@paypal/paypal-js/types/script-options";
 
+
+const paypalScriptOptions: PayPalScriptOptions = {
+    "client-id":
+        "AQaC8NCUwYIr1c9KY_7-qWf6JpBaUwaJ6ncFqFsJbq_89gviTwvOAPAgbyWpLuMHClr0zwgLPbbMSG5h",
+    currency: "USD"
+};
 
 const EditPaypalInfos: React.FC = () => {
 
@@ -19,12 +27,11 @@ const EditPaypalInfos: React.FC = () => {
         });
     }
 
-
     return (
         <PanelLayout>
             <div className="flex h-screen justify-center p-5">
 
-                    <div className="flex flex-col w-full mt-5 items-center">
+                <div className="flex flex-col w-full mt-5 items-center">
                     <label className="text-xl font-bold text-left pb-10">
                         Connect your PayPal account
                     </label>
@@ -33,6 +40,35 @@ const EditPaypalInfos: React.FC = () => {
                             className="flex flex-row space-x-5 items-center mb-5">
                             {paypalInfos?.email}
                         </label>
+
+                        <PayPalScriptProvider options={paypalScriptOptions}>
+
+                            <PayPalButtons
+                                // build a button to connect your paypal account or pay with a card
+                                style={{layout: "horizontal"}}
+                                createOrder={(data, actions) => {
+                                    return actions.order.create({
+                                        purchase_units: [
+                                            {
+                                                amount: {
+                                                    value: "1.00"
+                                                }
+                                            }
+                                        ]
+                                    });
+                                }}
+                                onApprove={(data, actions) => {
+                                    return actions.order.capture().then(function (details) {
+                                        alert('Transaction completed by ' + details.payer.name.given_name);
+                                    });
+                                }}
+                                onError={(err) => {
+                                    console.log(err);
+                                }}
+
+                            />
+                        </PayPalScriptProvider>
+
                     </div>
                 </div>
             </div>
